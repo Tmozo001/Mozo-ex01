5c18ea3008aa8cf4049e3404d524ff59ae49511c
Tyler Mozo <Tmozo001@gmail.com>
2021-09-05 19:09:07 -0400

    submission 2. added psuedo and copyright

--- src/main/java/exercise01/solution01.java
+++ src/main/java/exercise01/solution01.java
@@ -1,3 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Tyler  Mozo
+ */
+/*
+Psuedo
+ask user for their input with a print statement
+assign a scanner(name) to the user's keyboard input(System.in)
+print the string along with the required output.
+ */
 package exercise01;
 
 import java.util.Scanner;
